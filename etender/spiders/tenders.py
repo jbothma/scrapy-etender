71acfcaa2655e74bf608cdf5e2aedc2b6a2b74dc
JD Bothma <jbothma@gmail.com>
2019-04-05 20:41:50 +0200

    Scrape and log short description for tender from list page

--- etender/spiders/tenders.py
+++ etender/spiders/tenders.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from etender.items import EtenderItem, FileItem
 
 
 class TendersSpider(scrapy.Spider):
@@ -8,7 +9,23 @@ class TendersSpider(scrapy.Spider):
     start_urls = ['https://etenders.treasury.gov.za/content/advertised-tenders']
 
     def parse(self, response):
+        for tender_url in response.css('td.views-field-title a::attr(href)').getall():
+            meta = {
+                'description_short': response.css('td.views-field-title a::text').get(),
+            }
+            yield scrapy.Request(response.urljoin(tender_url),
+                                 callback=self.parse_tender,
+                                 meta=meta
+            )
+
         next_page = response.css('li.pager-next a::attr(href)').get()
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
+
+    def parse_tender(self, response):
+        etender = EtenderItem()
+        etender['url'] = response.url
+        etender['description_short'] = response.meta['description_short']
+
+        yield etender
