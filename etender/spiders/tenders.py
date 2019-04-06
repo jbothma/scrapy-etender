28f24950df6b37cda5dd7b20c5fb115411edfe48
JD Bothma <jbothma@gmail.com>
2019-04-06 09:10:41 +0200

    Scrape as much as possible from list page because tender page is unstructured

--- etender/spiders/tenders.py
+++ etender/spiders/tenders.py
@@ -9,11 +9,22 @@ class TendersSpider(scrapy.Spider):
     start_urls = ['https://etenders.treasury.gov.za/content/advertised-tenders']
 
     def parse(self, response):
-        for tender_url in response.css('td.views-field-title a::attr(href)').getall():
+        some_tender_row_title = response.css('td.views-field-title')[0]
+        tender_table = some_tender_row_title.xpath('../..')
+        for tender_row in tender_table.xpath('tr'):
+            url = tender_row.css('td.views-field-title a::attr(href)').get()
+            published_selector = 'td.views-field-field-date-published span::attr(content)'
+            briefing_selector = 'td.views-field-field-compulsory-briefing-sessio::text'
+            closing_date_selector = 'td.views-field-field-closing-date span::attr(content)'
             meta = {
-                'description_short': response.css('td.views-field-title a::text').get(),
+                'category': tender_row.css('td.views-field-field-tender-category::text').get().strip(),
+                'description_short': tender_row.css('td.views-field-title a::text').get().strip(),
+                'number': tender_row.css('td.views-field-field-code::text').get().strip(),
+                'date_published': tender_row.css(published_selector).get().strip(),
+                'closing_time_and_date': tender_row.css(closing_date_selector).get().strip(),
+                'compulsory_briefing_session': tender_row.css(briefing_selector).get().strip(),
             }
-            yield scrapy.Request(response.urljoin(tender_url),
+            yield scrapy.Request(response.urljoin(url),
                                  callback=self.parse_tender,
                                  meta=meta
             )
@@ -26,6 +37,12 @@ class TendersSpider(scrapy.Spider):
     def parse_tender(self, response):
         etender = EtenderItem()
         etender['url'] = response.url
+        etender['category'] = response.meta['category']
         etender['description_short'] = response.meta['description_short']
+        etender['number'] = response.meta['number']
+        etender['date_published'] = response.meta['date_published']
+        etender['closing_time_and_date'] = response.meta['closing_time_and_date']
+        etender['compulsory_briefing_session'] = response.meta['compulsory_briefing_session']
+        etender['free_text'] = response.css('div.field-name-field-econtact').get()
 
         yield etender
