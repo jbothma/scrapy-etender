b62348ad72f36ed922813839bd37c6bc9748c2ce
JD Bothma <jbothma@gmail.com>
2019-09-08 20:05:35 +0200

    WIP

--- etender/spiders/tenders.py
+++ etender/spiders/tenders.py
@@ -2,44 +2,69 @@
 import scrapy
 from etender.items import EtenderItem, FileItem
 import itertools
+import json
 
 
 class TendersSpider(scrapy.Spider):
     name = "tenders"
     allowed_domains = ["etenders.treasury.gov.za"]
-    start_urls = ["https://etenders.treasury.gov.za/content/advertised-tenders"]
 
-    def parse(self, response):
-        if response.url.endswith("/content/advertised-tenders"):
-            self.parse_advertised(response)
+    def start_requests(self):
+        yield scrapy.Request(
+            "https://etenders.treasury.gov.za/content/advertised-tenders",
+            callback=self.parse_search_start
+        )
+
+    def parse_search_start(self, response):
+        field_options = get_select_options("testing-dept", response)
+        for option in field_options:
+            if option["value"] == "All":
+                continue
+            if option["value"] != "ESKOM":
+                continue
+            yield self.create_search_request(option, page=0)
 
     def parse_advertised(self, response):
-        fields = ["tender-category", "region", "sector", "testing-dept", "tender-type"]
-        field_options = {}
-        for field in fields:
-            field_option_elements = response.css(f"#edit-field-{field}-tid option")
-            field_options[field] = [
-                (o.xpath("@value").get(), o.xpath("text()").get())
-                for o in field_option_elements
-            ]
+        print(response.meta["department_option"]["label"])
+        response_list = json.loads(response.body_as_unicode())
+        insert_command = [c for c in response_list if c["command"] == "insert"][0]
+        html = insert_command["data"]
+        html_response = scrapy.http.HtmlResponse(url="", body=html.encode("utf-8"))
+        for title in html_response.css(".views-field-title").xpath("/a/text()").getall():
+            print("  %s" % title)
+
+    def create_search_request(self, department_option, page):
         url = "https://etenders.treasury.gov.za/views/ajax"
         formdata = {
+            "field_tender_category_tid": "All",
+            "field_region_tid": "All",
+            "field_sector_tid": "All",
+            "field_testing_dept_tid": department_option["value"],
+            "field_tender_type_tid": "All",
             "view_name": "search_tender_published",
             "view_display_id": "block_2",
+            "view_path": "node/42",
+            "view_base_path": "search_tender_published",
+            "page": str(0),
         }
-        # for field, options in fieldoptions.items():
-        #   for option in options:
-        print(list(product_dict(**field_options)))
+        meta = {
+            "department_option": department_option,
+        }
+        return scrapy.http.FormRequest(
+            url,
+            formdata=formdata,
+            callback=self.parse_advertised,
+            method="POST",
+            meta=meta
+        )
 
-        #    formdata[f"field_{field_snakecase}_tid"] = option[0]
-        #   yielf scrapy.http.FormRequest(url, self.parse_advertised, "POST", formdata)
 
-    def parse_advertised_filter_result(self, response):
-        pass
-
-
-def product_dict(**kwargs):
-    keys = kwargs.keys()
-    vals = kwargs.values()
-    for instance in itertools.product(*vals):
-        yield dict(zip(keys, instance))
+def get_select_options(field_name, response):
+    field_option_elements = response.css(f"#edit-field-{field_name}-tid option")
+    return [
+        {
+            "value": o.xpath("@value").get(),
+            "label": o.xpath("text()").get(),
+        }
+        for o in field_option_elements
+    ]
